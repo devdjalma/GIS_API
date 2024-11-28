5cd7e9baa6994dfe792fe2f14cc3f3b8cd83285b
Djalma Filho <devdjalma@gmail.com>
2024-11-28 00:54:12 -0300

    Att: Models

--- gisapi/home/models.py
+++ gisapi/home/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Users(models.Model):
+    user_id = models.IntegerField(primary_key=True)
+    user_name = models.CharField(max_length=64)
+    user_email = models.CharField(max_length=64)
+    user_car_cod = models.CharField(max_length=64)
+    user_reg_date = models.DateTimeField(auto_created=True)
+    
