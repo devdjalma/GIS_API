b12b30eb332d7eeb131db5372ee19b68f26c801e
Djalma Filho <devdjalma@gmail.com>
2024-11-28 00:30:21 -0300

    Atualizando em tempo real

--- gisapi/home/views.py
+++ gisapi/home/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Home world!")
+    return HttpResponse("Home world Atualizado em tempo real!")
